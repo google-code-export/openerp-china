b4fe274920c156bc4942d66f9bec8a86363b08b0
wjfonhand@gmail.com <wjfonhand@gmail.com@a3a0cb34-e908-b5d8-2e62-3c3749468b4b>
2010-11-09 08:55:49 +0000

    Add authors name list

--- trunk/l10n_cn/__openerp__.py
+++ trunk/l10n_cn/__openerp__.py
@@ -18,28 +18,31 @@
 #
 ##############################################################################
 {
-    "name" : "OpenERP 基础中文化模块",
+    "name" : "",
     "version" : "1.1",
     "category": "Localisation/Account Charts",
     "author" : "openerp-china.org",
+    "maintainer":"openerp-china.org",
+    "contributors":[
+           "Digitalsatori",
+           "Oldrev",
+           "Xiao Xiang fu",
+           "Jeff",
+           "Joshua",
+           "Jack",
+           "Mrshelly",
+    ],
+    "website":"http://openerp-china.org",
+    "url":"http://code.google.com/p/openerp-china/source/browse/#svn/trunk/l10n_cn",
     "description": """
-===============================================================================
-    此模块用于为 OpenERP 的基础系统提供汉化，仅依赖内置的 base 模块。
-    
-    此模块包含如下功能：
-    1.  为 OpenERP 的 RML/PDF 报表提供中文字体支持
-    2.  为 OpenERP 添加中文省份数据
-    3.  本地化国家数据。
-    4.  本地化货币数据，并设置人民币为本币而不是默认的欧元。
-
-    中文说明：
-    使用文泉驿正黑体和 AR PL SungtiL GB 宋体（二者均为开源字体）替换系统原来不支持 Unicode 的英文字体，安装此模块可以使内置的报表自动支持中文字体。 
-    注意: 直接解压复制到 addons 目录. 然后更新模块列表并安装，此模块不能用导入功能安装。如果您使用的是 Windows 系统的话推荐使用 Windows 的 SimHei.ttf 和 SimSun.ttf 替换 fonts 目录中的相应字体文件。
-
-    原始 base_report_unicode 模块作者：Gábor Dukai
-
-    A relatorio reporting module to replace OpenERP's internal ODT reporting subsystem.
-        
+此模块包含如下功能：
+    1.  为 OpenERP 的 RML/PDF 报表提供中文字体支持。作者：Oldrev
+        原始 base_report_unicode 模块作者：Gábor Dukai
+    2.  为 OpenERP 添加中国省份数据。作者：Digitalsatori
+    3.  relatorio_report模块使OpenERP可以支持Calc格式的财务报表 作者:Oldrev
+    4.  科目类型\会计科目表模板\增值税\辅助核算类别\管理会计凭证簿\财务会计凭证簿 作者:Wjfonhand
+    5.  会计凭证界面本地化\凭证按月编号\凭证金额允许负数 作者:Wjfonhand
+    6.  科目显示全称\账簿打印\报表打印 作者:Joshua
     """,
     "depends" : ["base","account"],
     'init_xml': [
@@ -55,6 +58,7 @@
         'chart_cn/l10n_cn_wizard.xml',
     ],
     "license": "GPL-3",
+    "certificate":"",
     "active": False, 
     "installable": True
 }
